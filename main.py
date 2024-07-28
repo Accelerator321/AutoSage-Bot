658d8cd9897aa96125c7d67b2747246226c4743c
Shyam Mohan Gupta <shyamprime2610@gmail.com>
2024-07-28 10:02:20 +0000

    streamlit ui created

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import streamlit as st
 from dotenv import load_dotenv
-from utils import get_answer, llm, generate_answerbook, chain
-import warnings
+from utils import get_answer, isUrl, llm, generate_answerbook, chain
 
-# warnings.filters["all"]
-warnings.filterwarnings("ignore")
 import os
-
+if 'urls' not in st.session_state:
+  st.session_state.urls = [""] * 3
 load_dotenv()
+st.title("AutoSage 📃🔎")
+st.sidebar.title("Enter Your Urls here")
 
+main_placeholder = st.empty()
+status_placeholder = st.empty()
+result_placeholder = st.empty()
+source_placeholder = st.empty()
+
+for i in range(3):
+  st.session_state.urls[i] = st.sidebar.text_input(f"paste url-{i+1} here")
+button_clicked = st.sidebar.button("Generate Answerbook")
+
+if button_clicked:
+  print("clicked",st.session_state.urls )
+  urls_from_user= [url for url in st.session_state.urls if isUrl(url)]
+  if len(urls_from_user) !=0:
+    print(urls_from_user)
+    
+    st.session_state.db = generate_answerbook(urls=urls_from_user,element= main_placeholder)
+    st.header("Answer Book Generated✔️✔️")
+    
+
+
+st.session_state.query = main_placeholder.text_input("Enter Your Query Here And Press Enter")
+
+
+if st.session_state.query !="" and "db" in st.session_state:
+    
+    results = get_answer(st.session_state.query, st.session_state.db, status_placeholder)
+    print(results)
+    result_placeholder.subheader(results["result"])
+    source_placeholder.markdown(f"[{results['source']}]({results['source']})")
+    
 if __name__ == "__main__":
-  # print(llm.invoke("who is virat kohli tell in 10 words"))
-  book = generate_answerbook(urls=[ "https://en.wikipedia.org/wiki/Virat_Kohli"])
-  query= "what is age of virat kohli"
-  print(get_answer(query, book))
+  pass
+  # book = generate_answerbook(urls=[ "https://en.wikipedia.org/wiki/Virat_Kohli"])
+  # query= "what is age of virat kohli"
+  # print(get_answer(query, book))
   
   
--- utils.py
+++ utils.py
@@ -7,27 +7,25 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-import warnings
 import re
 
-warnings.filterwarnings("ignore")
-
-
 load_dotenv()
-
 
 llm = GoogleGenerativeAI(model="models/text-bison-001",
                          google_api_key=os.environ.get("GOOGLE_API_KEY"),
                          temperature=0.1)
 
 template = PromptTemplate(
-  input_variables=["query", "dataset"],
-  template = 'for the {query}. Please give me the most relevent result and source link from one of these given {dataset}. give results in xml format {{result, source}} field'
+    input_variables=["query", "dataset"],
+    template=
+    'for the {query}. Please give me the most relevent result and source link from one of these given {dataset}. give results in xml format {{result, source}} field'
 )
 
-chain = LLMChain(llm = llm, prompt=template, output_key = "result")
+chain = LLMChain(llm=llm, prompt=template, output_key="result")
 
-def generate_answerbook(urls):
+
+def generate_answerbook(urls, element):
+  element.text("Loading Data...✔️✔️")
   loader = UnstructuredURLLoader(urls=urls)
   data = loader.load()
 
@@ -36,9 +34,9 @@ def generate_answerbook(urls):
       chunk_size=200,
       chunk_overlap=0,
   )
-
+  element.text("Splitting Text...✔️✔️")
   doc = splitter.split_documents(data)
-
+  element.text("Generating Embeddings..✔️✔️")
   embeddings = HuggingFaceEmbeddings(
       model_name="sentence-transformers/all-MiniLM-L6-v2")
 
@@ -47,18 +45,29 @@ def generate_answerbook(urls):
   return index
 
 
-def get_answer(query, db):
+def get_answer(query, db, element):
   try:
-    res = chain.invoke({"query":query, "dataset" :db.similarity_search(query, k=4)})
+    element.subheader("Generating Answer.....✔️")
+    res = chain.invoke({
+        "query": query,
+        "dataset": db.similarity_search(query, k=4)
+    })
+
+    element.subheader("Answer Generated ✔️✔️")
+   
     result = re.search(r'<result>(.*?)</result>', res["result"]).group(1)
     source = re.search(r'<source>(.*?)</source>', res["result"]).group(1)
-    
-    return {"result": "result", source:source }
+
+    return {"result": result, "source": source}
 
   except Exception as e:
     print(e)
-    return {"result":" ", "source":" "}
-  
+    return {"result": " ", "source": " "}
 
+
+def isUrl(link):
+  regex= 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+' 
+  URL= re.findall(regex,link) 
+  return len(URL)>0 
 if __name__ == "__main__":
   print(llm.invoke("who is virat kohli tell in 10 words"))
